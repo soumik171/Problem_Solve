0755a0b85e236e83a2c29f54a128de36582ad886
soumik171 <112255552+soumik171@users.noreply.github.com>
2025-06-16 01:48:51 +0600

    Update search-in-rotated-sorted-array.js

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -11,14 +11,18 @@ var search = function (nums, target) {
     let mid = Math.floor((low + high) / 2);
 
     if (nums[mid] == target) return mid;
-
+    
+    //if left sort
     if (nums[low] <= nums[mid]) {
       if (nums[low] <= target && target <= nums[mid]) {
         high = mid - 1;
       } else {
         low = mid + 1;
       }
-    } else {
+    } 
+      
+    //if right sort
+    else {
       if (nums[mid] <= target && target <= nums[high]) {
         low = mid + 1;
       } else {
