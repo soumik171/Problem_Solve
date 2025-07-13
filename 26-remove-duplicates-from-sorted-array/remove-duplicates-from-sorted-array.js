40d64b4cb490806edac12f2240f74810b02df4aa
soumik171 <112255552+soumik171@users.noreply.github.com>
2025-07-13 22:46:25 +0600

    Time: 0 ms (100.00%) | Memory: 58 MB (39.77%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -2,16 +2,16 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
-    let count = 0;
+//using double pointer:
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== nums[i + 1]) {
-            nums[count] = nums[i];
-            count++;
-        }
+// take i at 0th index and j at 1st index; if arr[j] and arr[i] are different ,if true then insert arr[j] into arr[i+1] and increase the value of i pointer 
+var removeDuplicates = function (nums) {
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      nums[i + 1] = nums[j];
+      i++;
     }
-
-    return count;
-    
+  }
+  return i + 1; // length
 };
