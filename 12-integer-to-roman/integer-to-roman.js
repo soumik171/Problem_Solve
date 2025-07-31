134c2ad6b274b09264b105be356af6d9714b0f4f
soumik171 <112255552+soumik171@users.noreply.github.com>
2025-07-31 12:14:17 +0600

    Time: 2 ms (98.29%) | Memory: 60.6 MB (88.26%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.js
+++ 12-integer-to-roman/integer-to-roman.js
@@ -1,44 +1,28 @@
-/**
- * @param {number} num
- * @return {string}
- */
 let intToRoman = (num) => {
-  let store = {
-    1000: "M",
-    900: "CM",
-    500: "D",
-    400: "CD",
-    100: "C",
-    90: "XC",
-    50: "L",
-    40: "XL",
-    10: "X",
-    9: "IX",
-    5: "V",
-    4: "IV",
-    1: "I",
-  };
-  // Obj always keeps data in sorted order,if i am changing randomly,also it's sorted it's data, as we require large to small, so we iterate the keys to large to small
+  const val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const roman = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
+  let res = "";
+  for (let i = 0; i < val.length; i++) {
+    let times = Math.floor(num / val[i]);
 
-  let res = [];
-  for (let key of Object.keys(store).sort((a, b) => b - a)) {
-    key = Number(key);
-    let times = Math.floor(num / key);
-
-    // while (times--) {
-    //   res += store[key];
-    // }
-    // num = num % key;
-    // or,
-
-    //if using repeat, then no need to use outside while loop, cz it's alreadykeep iterating till the number of times
-
-    if (times > 0) {
-      res.push(store[key].repeat(times)); 
-      num = num % key;
+    while (times--) {
+      res += roman[i];
     }
+    num = num % val[i];
   }
-  return res.join("");
+  return res;
 };
-
-
