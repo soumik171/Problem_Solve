d14597cf35462d9477e47abab5cd046e1b5d0107
soumik171 <112255552+soumik171@users.noreply.github.com>
2025-07-31 12:45:36 +0600

    Time: 3 ms (90.74%) | Memory: 60.4 MB (83.22%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.js
+++ 13-roman-to-integer/roman-to-integer.js
@@ -2,25 +2,21 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    let res = 0;
-    const roman = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    };
-
-    for (let i = 0; i < s.length - 1; i++) {
-        if (roman[s[i]] < roman[s[i + 1]]) {
-            res -= roman[s[i]];
-        } else {
-            res += roman[s[i]];
-        }
-    }
-
-    return res + roman[s[s.length - 1]];    
+let romanToInt = (str) => {
+  const roman = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let result = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (i == str.length - 1 || roman[str[i]] >= roman[str[i + 1]]) {
+      result += roman[str[i]];
+    } else result -= roman[str[i]];
+  }
+  return result;
 };
